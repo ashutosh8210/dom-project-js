6e7b9c6ddd521c4f23c187bb411b4c636a9d0301
ashutosh8210 <rajashutosh556@gmail.com>
2025-10-05 13:09:02 +0530

    first project complete

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,12 +1,9 @@
 const buttons = document.querySelectorAll('.button');
-// console.log(buttons);
 const body = document.body;
 
-buttons.forEach(function (button) {
-  // console.log(button);
+buttons.forEach(function (button) { 
+  
   button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target.id);
     if (e.target.id) {
       body.style.backgroundColor = e.target.id;
     }
