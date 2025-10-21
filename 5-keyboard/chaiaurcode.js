c505f688ad36a3c331ab1cbc9877bf92acf89052
ashutosh8210 <rajashutosh556@gmail.com>
2025-10-21 16:54:37 +0530

    project 5 and 6  complete

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
 console.log('Project 5');
+const insert = document.getElementById('insert');
+window.addEventListener('keydown', function (e) {
+  console.log(e);
+  insert.innerHTML = `
+  <div class='color'>
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}s</td>
+    <td>${e.code}</td>a
+  </tr>
+</table> 
+  </div>`;
+});
